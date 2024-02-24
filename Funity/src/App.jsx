db80ce07b967cedf2eb3fb17b0be8a5ef416f701
John Patrick <furaquejp11@gmail.com>
2024-02-24 21:30:38 +0800

    first commit

---
+++ Funity/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='h-screen flex justify-center items-center'>
+      <div className=' bg-slate-900 size-64'>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
