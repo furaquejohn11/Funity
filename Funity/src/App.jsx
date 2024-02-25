9512303de2a7e815d88986223e6c323897653bef
MisaJC <johncarlomisa399@gmail.com>
2024-02-25 23:38:29 +0800

    added nav.jsx inside components folder and styles at app.css

--- Funity/src/App.jsx
+++ Funity/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Nav from './components/Nav'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className='h-screen bg-gradient-to-b from-blue-800 via-transparent to-gray-900 flex justify-center items-center'>
+      <Nav/>
       <h1 className='text-blue-500 text-7xl'>Funity - SDG 17 Hub</h1>
     </div>
   )
